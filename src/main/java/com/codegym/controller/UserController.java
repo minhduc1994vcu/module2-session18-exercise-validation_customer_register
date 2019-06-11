993dda1abfd4c2f0ef4113931f7b1a57b8b8f8b4
minhduc <minhduc1994vcu@gmail.com>
2019-06-11 22:43:06 +0700

    function update

--- src/main/java/com/codegym/controller/UserController.java
+++ src/main/java/com/codegym/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -30,15 +31,36 @@ public class UserController {
     }
 
     @PostMapping("/user/create")
-    public String save(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+    public String save(@Valid @ModelAttribute("user") User user,Model model, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
         new User().validate(user, bindingResult);
         if (bindingResult.hasFieldErrors()) {
+            model.addAttribute("user", new User());
+
             return "create";
         } else {
             int id = this.userService.findAll().size() + 1;
             user.setId(id);
             this.userService.save(user);
             redirectAttributes.addFlashAttribute("success", "Create new user successfully");
+            return "redirect:/";
+        }
+    }
+
+    @GetMapping("/user/{id}/update")
+    public String update(Model model, @PathVariable int id) {
+        User user = this.userService.findById(id);
+        model.addAttribute("user", user);
+        return "update";
+    }
+
+    @PostMapping("/user/update")
+    public String update(@Valid @ModelAttribute("user") User user, BindingResult bindingResult, RedirectAttributes redirectAttributes) {
+        new User().validate(user, bindingResult);
+        if (bindingResult.hasFieldErrors()) {
+            return "update";
+        } else {
+            this.userService.update(user.getId(), user);
+            redirectAttributes.addFlashAttribute("success", "Update customer's information successfully");
             return "redirect:/";
         }
     }

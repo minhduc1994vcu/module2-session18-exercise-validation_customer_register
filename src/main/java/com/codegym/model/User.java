143e2e9e1380fc3e980710c8deae1d4d8e359506
minhduc <minhduc1994vcu@gmail.com>
2019-06-11 22:15:14 +0700

    model user

---
+++ src/main/java/com/codegym/model/User.java
@@ -0,0 +1,123 @@
+package com.codegym.model;
+
+import org.springframework.stereotype.Component;
+import org.springframework.validation.Errors;
+import org.springframework.validation.ValidationUtils;
+import org.springframework.validation.Validator;
+
+import javax.validation.constraints.Min;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Size;
+@Component
+public class User implements Validator {
+    private int id;
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+    private int age;
+    private String email;
+
+    public User() {
+    }
+
+    public User(int id, String firstName, String lastName, String phoneNumber, int age, String email) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+        this.age = age;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public boolean supports(Class<?> clazz) {
+        return User.class.isAssignableFrom(clazz);
+    }
+
+    @Override
+    public void validate(Object target, Errors errors) {
+        User user = (User) target;
+        String firstName = user.getFirstName();
+        ValidationUtils.rejectIfEmpty(errors, "firstName", "firstName.empty");
+        if (firstName.length() > 45 || firstName.length() < 5) {
+            errors.rejectValue("firstName", "firstName.length");
+        }
+
+        String lastName = user.getLastName();
+        ValidationUtils.rejectIfEmpty(errors, "lastName", "lastName.empty");
+        if (lastName.length() > 45 || lastName.length() < 5) {
+            errors.rejectValue("lastName", "lastName.length");
+        }
+
+        String phoneNumber = user.getPhoneNumber();
+        ValidationUtils.rejectIfEmpty(errors, "phoneNumber", "phoneNumber.empty");
+        if (phoneNumber.length() > 11 || phoneNumber.length() < 10) {
+            errors.rejectValue("phoneNumber", "phoneNumber.length");
+        }
+        if (!phoneNumber.startsWith("0")) {
+            errors.rejectValue("phoneNumber", "phoneNumber.startsWith");
+        }
+        if (!phoneNumber.matches("(^$|[0-9]*$)")) {
+            errors.rejectValue("phoneNumber", "phoneNumber.matches");
+        }
+
+        int age = user.getAge();
+        if (age < 18) {
+            errors.rejectValue("age", "age.min");
+        }
+
+        String email = user.getEmail();
+        ValidationUtils.rejectIfEmpty(errors, "email", "email.empty");
+        if (!email.matches("(^[A-Za-z0-9]+[A-Za-z0-9]*@[A-Za-z0-9]+(\\.[A-Za-z0-9]+)$)")) {
+            errors.rejectValue("email", "email.matches");
+        }
+    }
+}

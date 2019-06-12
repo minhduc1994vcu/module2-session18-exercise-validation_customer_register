7c29edd21b25a268b8b2361fae757fa93fb7fcde
minhduc <minhduc1994vcu@gmail.com>
2019-06-12 09:29:43 +0700

    bean message sources

--- src/main/java/com/codegym/ApplicationConfig.java
+++ src/main/java/com/codegym/ApplicationConfig.java
@@ -3,9 +3,11 @@ package com.codegym;
 import org.springframework.beans.BeansException;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationContextAware;
+import org.springframework.context.MessageSource;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.support.ResourceBundleMessageSource;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 import org.thymeleaf.TemplateEngine;
@@ -49,5 +51,13 @@ public class ApplicationConfig extends WebMvcConfigurerAdapter implements Applic
         ThymeleafViewResolver viewResolver = new ThymeleafViewResolver();
         viewResolver.setTemplateEngine(templateEngine());
         return viewResolver;
+    }
+
+    //error
+    @Bean
+    public MessageSource messageSource() {
+        ResourceBundleMessageSource messageSource = new ResourceBundleMessageSource();
+        messageSource.setBasenames("ValidationMessages");
+        return messageSource;
     }
 }
